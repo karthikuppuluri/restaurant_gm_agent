cb4788563a70ccc04f25c54da6fed10d7abb3ad2
Krishna Tej Bhat <krishnatej.bhat@gmail.com>
2026-06-07 21:41:41 -0400

    added google api test

---
+++ test_gemini.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from google import genai
+
+# print("env api key: ", os.environ['GOOGLE_API_KEY'])
+
+client = genai.Client(
+    vertexai=True,
+    project=os.environ['GOOGLE_CLOUD_PROJECT'],
+    location=os.environ['GOOGLE_CLOUD_LOCATION'],
+)
+response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents="What is the capital of France?",
+)
+print(response.text)
